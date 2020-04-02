be3eee8f06e7fcb5066b59debec4d66900bf9df0
Thomas Stanley <7205280+thomasastanley@users.noreply.github.com>
2020-04-02 15:18:11 -0400

    version 1.1
    extends model to 60 N-S as described at https://www.nat-hazards-earth-syst-sci-discuss.net/nhess-2019-434/

--- API.r
+++ API.r
@@ -1,27 +1,27 @@
-# 7-day ARI calculation v1.0
-# 11/20/2018
+# 7-day ARI calculation v1.1
+# 2020-4-2
 # Thomas Stanley NASA GSFC/GESTAR/USRA
-# Calculates a 7-day antecedent precipitation index
+# Calculates a 7-day antecedent rainfall index
 
 # Load R packages
 library(raster)
 
 # Set working directory
 setwd('C:/LHASA')
-# List all daily precipitation files
+# List all daily rainfall files
 files <- list.files(path='IMERG', full.names=TRUE)
 
-# Set antecedent precipitation window
+# Set antecedent rainfall window
 API.window <- 7
 # Set IDW exponent
 exponent <- 2
 # Calculate weights
-w <- 1/seq(API.window,1)^exponent
+w <- 1/seq(API.window, 1)^exponent
 # Iterate through every day, starting at the end of the 1st API window
 for(day in API.window:length(files)){
 	# Open files within window, including current day
-	IMERG <- crop(stack(files[(day-API.window+1):day]), extent(-180,180,-50,50))/10
-	# Calculate antecedent precipitation index
+	IMERG <- crop(stack(files[(day - API.window + 1):day]), extent(-180, 180, -60, 60)) / 10
+	# Calculate antecedent rainfall index
 	API <- calc(w*IMERG,sum)/sum(w)
 	# Save to disk
 	writeRaster(API,filename=gsub('IMERG/','API/',files[day]))
